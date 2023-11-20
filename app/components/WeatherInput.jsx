eae11381f1e0769a1876fb9dbbcaf53fa8989b28
Kristian Demko <demkokristian@gmail.com>
2023-11-20 21:25:27 +0100

    change input to form input

--- app/components/WeatherInput.jsx
+++ app/components/WeatherInput.jsx
@@ -4,25 +4,27 @@ import { getWeather } from "../data/weather";
 
 export default function WeatherInput({ setData }) {
   const [input, setinput] = useState("");
-  const handleCity = async () => {
+  const handleSubmit = async () => {
     const weather = await getWeather(input);
     setData(weather);
   };
   return (
     <div className="w-full">
-      <input
-        type="text"
-        placeholder="type city"
-        className="w-full focus:outline-none focus:border-gray-800 border-1 font-medium p-3 text-xl rounded-md text-center"
-        value={input}
-        onChange={(e) => setinput(e.target.value)}
-      />
-      <button
-        className="w-full text-white text-xl border hover:bg-blue-400 border-blue-600 font-bold p-3 bg-blue-500 my-3 rounded-md"
-        onClick={handleCity}
-      >
-        Search
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="type city"
+          className="w-full focus:outline-none focus:border-gray-800 border-1 font-medium p-3 text-xl rounded-md text-center"
+          value={input}
+          onChange={(e) => setinput(e.target.value)}
+        />
+        <button
+          className="w-full text-white text-xl border hover:bg-blue-400 border-blue-600 font-bold p-3 bg-blue-500 my-3 rounded-md"
+          onClick={handleCity}
+        >
+          Search
+        </button>
+      </form>
     </div>
   );
 }
